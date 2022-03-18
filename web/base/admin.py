0d5d209c07864d81fa45fc5b095a4e71c222779e
Wajuma <meltusjuma5@gmail.com>
2022-03-18 12:05:06 +0300

    register models into admin panel

--- web/base/admin.py
+++ web/base/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Topic, Room, Message
 
 # Register your models here.
+admin.site.register(Topic)
+admin.site.register(Room)
+admin.site.register(Message)
