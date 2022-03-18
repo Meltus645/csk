c14f60cc2872f1736d1736e747f4e3eb26c8f5db
Wajuma <meltusjuma5@gmail.com>
2022-03-18 19:11:34 +0300

    added roo, room-create, room-updated routes

--- web/base/urls.py
+++ web/base/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
-from django.views import View
+from django.urls import path 
 from . import views
 
 # base app urls
 urlpatterns:list = [
     path('', views.index, name ='home'),
+
+    path('room/<str:pk>', views.room, name ='room'),
+    path('create-room/', views.createRoom, name ='create-room'),
+    path('update-room/<str:pk>', views.updateRoom, name ='update-room'),
 ]
