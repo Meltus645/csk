314aa1bf280de50821f656fc5f2e23a067b085d8
Wajuma <meltusjuma5@gmail.com>
2022-03-18 19:13:36 +0300

    Added create, update views
    user can now create a chat room
    user can now update chat room details

--- web/base/views.py
+++ web/base/views.py
@@ -1,11 +1,46 @@
-from django.shortcuts import render 
+from django.shortcuts import render, redirect 
+from .forms import RoomForm
+from django.db.models import Q
 from .models import Topic, Room 
 
 # Create your views here.
 
 def index(request):
+    q =request.GET.get('q') if request.GET.get('q') != None else ''
     topics =Topic.objects.all()
-    rooms =Room.objects.all()
+    rooms =Room.objects.filter(
+        Q(topic__name__icontains =q)|
+        Q(name__icontains =q) |
+        Q(description__icontains =q)
+    )
 
     context:dict ={'rooms': rooms, 'topics': topics}
     return render(request, 'base/index.html', context)
+
+def room(request, pk):
+    room =Room.objects.get(id =pk) 
+    participants =room.participants.all()
+    context: dict ={'room': room, 'partcipants': participants}
+    return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form =RoomForm()
+    if request.method =='POST':
+        form =RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context ={'form': form}
+    return render(request, 'base/room_form.html', context)    
+
+def updateRoom(request, pk):
+    room =Room.objects.get(id =pk)
+    form =RoomForm(instance=room)
+    if request.method =='POST':
+        form =RoomForm(request.POST, instance =room)
+        if form.is_valid():
+            form.save() 
+            return redirect('home') 
+    context ={'form': form}
+    return render(request, 'base/room_form.html', context)    
