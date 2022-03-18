7e146c2f38b7b54673772dc74802e289f27995dd
Wajuma <meltusjuma5@gmail.com>
2022-03-18 14:38:41 +0300

    create context

--- web/base/views.py
+++ web/base/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render 
+from .models import Topic, Room 
 
 # Create your views here.
 
 def index(request):
-    context:dict ={}
+    topics =Topic.objects.all()
+    rooms =Room.objects.all()
+
+    context:dict ={'rooms': rooms, 'topics': topics}
     return render(request, 'base/index.html', context)
