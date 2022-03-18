ebb51b0ebfddb9a3ad128d49c2323cb1097b7e06
Wajuma <meltusjuma5@gmail.com>
2022-03-18 19:10:17 +0300

    updated models
    Added participants key,
    Added ordering

--- web/base/models.py
+++ web/base/models.py
@@ -11,26 +11,26 @@ class Topic(models.Model):
 
 class Room(models.Model):
     host =models.ForeignKey(User, on_delete=models.SET_NULL, null =True)
-    topic =models.ForeignKey(Topic, on_delete=models.CASCADE)
+    topic =models.ForeignKey(Topic, on_delete=models.SET_NULL, null= True)
     name =models.CharField(max_length= 200)
     description =models.TextField(null= True, blank= True)
+    participants =models.ManyToManyField(User, related_name='participants')
     updated =models.DateTimeField(auto_now=True)
-    created =models.DateTimeField(auto_now_add=True)
-    # participants =
-
-    def __str__(self)->str:
-        return self.name
+    created =models.DateTimeField(auto_now_add=True) 
 
     class Meta:
-        ordering  =['-updated', '-created']   
-    
+        ordering  =['-updated', '-created']
+
+    def __str__(self)->str:
+        return self.name   
 
 class Message(models.Model):
     sender =models.ForeignKey(User, on_delete= models.CASCADE)
+    room =models.ForeignKey(Room, on_delete= models.CASCADE, null =True)
     message =models.TextField()
     updated =models.DateTimeField(auto_now=True)
     created =models.DateTimeField(auto_now_add=True) 
 
     def __str__(self):
-        return self.message[:50]
+        return self.message[0:50]
     
